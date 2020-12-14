eec2ba3c787415d0b5c292d1cc1e92678c13cf35
nishant <em68wj@ing.com>
2020-12-14 08:35:24 +0100

    add exercise Printable

--- src/main/scala/day3/TypeClass/Printable.scala
+++ src/main/scala/day3/TypeClass/Printable.scala
@@ -1,18 +1,14 @@
 package day3.TypeClass
 
-// A trait with one type parameter(can be more)
-// An abstract behavior for the generic type
-// Instances of this trait for specific types
-// 1. Place them in an object (import)
-// 2. Place them in a trait (inheritance)
-// 3. Place them in the companion object of type class
-// 4. Place them in the companion object of the type
-// Exposing the functionality
-// cats.instances
-// cats.syntax
-// cats._
-// cats.implicits._
-
-object DD {
-
-}
+//Exercise
+// 1. Define a typeclass Printable[A] containing a single method format.
+//    format should accept a value of type A and return a String.
+// 2. Create an object PrintableInstances containing instances of Printable for String and Int.
+// 3. Define an object Printable with a generic interface method:
+//    format accepts a value of type A and a Printable of the corresponding type.
+//    It uses the relevant Printable to convert the A to a String.
+// 4. Create an object called PrintableSyntax.
+// 5. Inside PrintableSyntax define an implicit class Print- ableOps[A] to wrap up a value of type A.
+// 6. In Printable Ops define the following method:
+//    format accepts an implicit Printable[A] and returns a String representation of the wrapped A;
+// 7. Use extension methods to print Employee
