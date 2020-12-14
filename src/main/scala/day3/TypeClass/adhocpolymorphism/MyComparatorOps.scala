8fdc66717c5a6fdca8de96f9f366047d34bfa750
nishant <em68wj@ing.com>
2020-12-14 07:14:15 +0100

    add description for laws

--- src/main/scala/day3/TypeClass/adhocpolymorphism/MyComparatorOps.scala
+++
@@ -1,7 +0,0 @@
-package day3.TypeClass.adhocpolymorphism
-
-object MyComparatorOps {
-//  implicit class RichMyComparator[T : MyComparator](t1: T) {
-//    def compare(t2: T): Int = MyComparator[T].compare(t1, t2)
-//  }
-}
